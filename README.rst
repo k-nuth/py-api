d4ca327de67c3f66ad0eafb42355871a2996794c
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-07-14 10:09:31 -0300

    visibility changes

---
+++ README.rst
@@ -0,0 +1,16 @@
+# Bitcoin/Litecoin Full-Node Library written in Python
+
+[![Build Status](https://travis-ci.org/bitprim/bitprim.svg?branch=master)](https://travis-ci.org/bitprim/bitprim) [![Appveyor Status](https://ci.appveyor.com/api/projects/status/github/bitprim/bitprim?branch=master&svg=true)](https://ci.appveyor.com/project/hanchon/bitprim?branch=master) 
+
+# bitprim
+Bitcoin development platform for Python Applications
+
+## Getting started 
+
+```sh
+$ pip install bitprim
+```
+
+## Reference documentation ##
+
+For more detailed documentation, please refer to https://www.bitprim.org/
--- bitprim/setup.py
+++ bitprim/setup.py
@@ -1,18 +1,43 @@
-# pip install wheel
-# pip install twine
+ # 
+ # Copyright (c) 2017 Bitprim developers (see AUTHORS)
+ # 
+ # This file is part of Bitprim.
+ # 
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU Affero General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ # 
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU Affero General Public License for more details.
+ # 
+ # You should have received a copy of the GNU Affero General Public License
+ # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ # 
 
-
-# pip install  -e .
-# python setup.py bdist_wheel --universal
-# twine upload dist/*
-
-# -------------------------------------------
-
-
-# from distutils.core import setup, Extension
 from setuptools import setup, find_packages
-from setuptools.extension import Extension
 import platform
+
+
+install_requires = [
+    "bitprim-native >= 1.0.1",
+]
+
+
+# install_requires = [
+#     "tqdm >= 4.14",
+#     "pkginfo >= 1.0",
+#     "requests >= 2.5.0, != 2.15, != 2.16",
+#     "requests-toolbelt >= 0.8.0",
+#     "setuptools >= 0.7.0",
+# ]
+
+# if sys.version_info[:2] < (2, 7):
+#     install_requires += [
+#         "argparse",
+#     ]
 
 
 setup(
@@ -20,17 +45,39 @@ setup(
     version='1.0.2',
 
     description='Bitprim Platform',
-    long_description='Bitprim Platform',
+    long_description=open("../README.rst").read(),
     url='https://github.com/bitprim/bitprim-py',
 
     # Author details
     author='Bitprim Inc',				#TODO!
     author_email='dev@bitprim.org',		#TODO!
-
-    # Choose your license
     license='MIT',    					#TODO!
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # classifiers=[
+    #     # How mature is this project? Common values are
+    #     #   3 - Alpha
+    #     #   4 - Beta
+    #     #   5 - Production/Stable
+    #     'Development Status :: 3 - Alpha',
+
+    #     # Indicate who your project is intended for
+    #     'Intended Audience :: Developers',
+    #     'Topic :: Software Development :: Build Tools',
+
+    #     # Pick your license as you wish (should match "license" above)
+    #     'License :: OSI Approved :: MIT License',
+
+    #     # Specify the Python versions you support here. In particular, ensure
+    #     # that you indicate whether you support Python 2, Python 3 or both.
+    #     'Programming Language :: Python :: 2',
+    #     'Programming Language :: Python :: 2.7',
+    #     'Programming Language :: Python :: 3',
+    #     'Programming Language :: Python :: 3.3',
+    #     'Programming Language :: Python :: 3.4',
+    #     'Programming Language :: Python :: 3.5',
+    # ],
+
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -38,22 +85,24 @@ setup(
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
 
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
+        "Intended Audience :: Developers",
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],    
 
     # What does your project relate to?
     keywords='bitcoin litecoin money bitprim',
@@ -64,18 +113,13 @@ setup(
     # packages=['bitprim'],
 
 
-
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
     #   py_modules=["my_module"],
     py_modules=["bitprim"],
 
-    # # List run-time dependencies here.  These will be installed by pip when
-    # # your project is installed. For an analysis of "install_requires" vs pip's
-    # # requirements files see:
-    # # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=['peppercorn'],
-    install_requires=['bitprim-native'],
+    # install_requires=['bitprim-native'],
+    install_requires=install_requires,
 
 
     # List additional groups of dependencies here (e.g. development
