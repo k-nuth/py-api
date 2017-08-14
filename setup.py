b6384c2b6bb82996a984a87b576332634783cd95
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-08-14 15:43:43 -0300

    fix

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     name = __title__,
     version = __version__,
     description = __summary__,
-    long_description=open("../README.rst").read(),
+    long_description=open("./README.rst").read(),
     license = __license__,
     url = __uri__,
     author = __author__,
