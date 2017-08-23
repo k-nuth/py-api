d1abbcfb819d5c690b66cde0186db6209000a621
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-23 16:11:39 -0300

    docstring tests

---
+++ generate_docs.bat
@@ -0,0 +1,6 @@
+@echo off
+
+REM pip install pydoc-markdown
+REM pip install git+https://github.com/NiklasRosenstein/pydoc-markdown.git  # latest development version
+
+pydocmd simple bitprim+ > docs.md
