87efce057cf6e7bcfcbc2adf78e33dbb1c24897e
Dario Ramos <dario.fenix@gmail.com>
2017-09-14 17:55:56 -0300

    DEV-45: Script worked in Python 2.x but not in 3.x, the latter of which Travis uses

--- doc/remove_self.py
+++ doc/remove_self.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Remove "self" parameter from Doxygen-generated XML documentation
 """
@@ -8,12 +8,12 @@ import os
 import sys
 
 if len(sys.argv) != 2:
-    print "Usage: remove_self.py <xml_dir>"
+    print("Usage: remove_self.py <xml_dir>")
     sys.exit(1)
 
 os.chdir(sys.argv[1])
 for class_doc in glob.glob("*.xml"):
-    print class_doc
+    print(class_doc)
     lines_to_write = []
     with open(class_doc, 'r') as current_file:
         read_lines = current_file.readlines()
