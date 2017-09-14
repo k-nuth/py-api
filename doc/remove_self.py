9e96e72b9df183141c0a93e3d359de32e2f5a933
Dario Ramos <dario.fenix@gmail.com>
2017-09-14 17:35:04 -0300

    DEV-45: Remove self parameter

--- doc/remove_self.py
+++ doc/remove_self.py
@@ -14,6 +14,22 @@ if len(sys.argv) != 2:
 os.chdir(sys.argv[1])
 for class_doc in glob.glob("*.xml"):
     print class_doc
+    lines_to_write = []
     with open(class_doc, 'r') as current_file:
-        lines = current_file.readlines()
-        print len(lines)
+        read_lines = current_file.readlines()
+        i = 0
+        while i < len(read_lines):
+            if read_lines[i].strip() == "<param>":
+                if "self" in read_lines[i+1].strip():
+                    i = i + 4
+                else:
+                    lines_to_write.append(read_lines[i])
+                    i = i + 1
+            elif "(self" in read_lines[i]:
+                lines_to_write.append(read_lines[i].replace("(self,", "(").replace("(self", "("))
+                i = i + 1
+            else:
+                lines_to_write.append(read_lines[i])
+                i = i + 1
+    with open(class_doc, 'w') as modified_file:
+        modified_file.writelines(lines_to_write)
